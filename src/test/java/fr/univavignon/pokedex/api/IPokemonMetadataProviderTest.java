60cc2c3233396969daa238d66c3c96e5fc26cb29
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-05 03:54:51 +0100

    Test Metadonnees avec index Invalid

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,6 +1,5 @@
 package fr.univavignon.pokedex.api;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -14,35 +13,32 @@ public class IPokemonMetadataProviderTest {
     @Mock
     private IPokemonMetadataProvider metadataProvider;
 
-//pour que je teste un index Valid
-    //avec le pokemon Bulbizarre
+    // Test pour un index valide avec le Pokémon Bulbizarre
     @Test
     public void getPokemonMetadata_AvecValidIndexBulbizarre() throws PokedexException {
-        Mockito.when(metadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        when(metadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
         PokemonMetadata metadata = metadataProvider.getPokemonMetadata(0);
         assertNotNull(metadata);
         assertEquals("Bulbizarre", metadata.getName());
     }
-    //avec le pokemon Aquali
-    // ici, je teste  plus de parametres
+
+    // Test pour un index valide avec le Pokémon Aquali
     @Test
     public void getPokemonMetadata_AvecValidIndexAquali() throws PokedexException {
-
         int indexReal = 133;
         String nomReal = "Aquali";
         int attaqueReal = 186;
         int defenseReal = 168;
         int enduranceReal = 260;
 
-        Mockito.when(metadataProvider.getPokemonMetadata(133)).thenReturn(new PokemonMetadata(indexReal, nomReal, attaqueReal, defenseReal, enduranceReal));
+        when(metadataProvider.getPokemonMetadata(133)).thenReturn(new PokemonMetadata(indexReal, nomReal, attaqueReal, defenseReal, enduranceReal));
         PokemonMetadata metadata2 = metadataProvider.getPokemonMetadata(133);
-        //verification:
-        assertNotNull(metadata2, "METADATA  ne doit pas être null !!!");
+
+        // Vérifications
+        assertNotNull(metadata2, "METADATA ne doit pas être null !!!");
         assertEquals("Aquali", metadata2.getName(), "Le nom doit être : Aquali !!!");
-        assertEquals(186, metadata2.getAttack(), " Aquali attack doit être : 186 !!!");
-        assertEquals(168, metadata2.getDefense(), "Aquali defense doit être : 168 !!! ");
+        assertEquals(186, metadata2.getAttack(), "Aquali attack doit être : 186 !!!");
+        assertEquals(168, metadata2.getDefense(), "Aquali defense doit être : 168 !!!");
         assertEquals(260, metadata2.getStamina(), "Aquali endurance doit être : 260 !!!");
     }
-
-
 }
