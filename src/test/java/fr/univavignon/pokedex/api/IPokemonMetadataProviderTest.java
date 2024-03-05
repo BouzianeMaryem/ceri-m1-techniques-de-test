b068f741c91e610d025a02fd9fde96a3a866ee32
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-05 03:26:26 +0100

    Test Metadonnees avec index Invalid

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,14 +1,15 @@
 package fr.univavignon.pokedex.api;
 
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 public class IPokemonMetadataProviderTest {
 
     private IPokemonMetadataProvider metadataProvider;
