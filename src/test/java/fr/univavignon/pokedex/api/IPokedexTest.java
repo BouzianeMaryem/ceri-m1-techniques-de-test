d48488580ec20411256ab9cbe7df938478b14784
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-05 06:25:41 +0100

    IPokedex : test get pokemons avec order

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
