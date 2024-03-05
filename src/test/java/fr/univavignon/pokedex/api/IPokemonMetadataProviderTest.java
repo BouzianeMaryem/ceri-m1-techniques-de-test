637fe0d8d487d1b812edd30cd4f2ce96cdb7c0ec
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-05 01:12:58 +0100

    PokemonMetadataProvider"Test valid index avec Bulbizarre"

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,2 +1,27 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IPokemonMetadataProviderTest {
+
+    private IPokemonMetadataProvider metadataProvider;
+
+    @BeforeEach
+    public void setUp() {
+        metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+    }
+//pour que je teste un index Valid
+//avec le pokemon Bulbizarre
+    @Test
+    public void getPokemonMetadata_AvecValidIndexBulbizarre() throws PokedexException {
+        Mockito.when(metadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        PokemonMetadata metadata = metadataProvider.getPokemonMetadata(0);
+        assertNotNull(metadata);
+        assertEquals("Bulbizarre", metadata.getName());
+    }
+
+
 }
