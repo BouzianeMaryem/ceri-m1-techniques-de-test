978395e5ff7ca8f021caf232fa7880890b7ad3ba
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-05 02:36:44 +0100

    Test Metadonnees avec Aquili

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -22,5 +22,20 @@ public class IPokemonMetadataProviderTest {
         assertNotNull(metadata);
         assertEquals("Bulbizarre", metadata.getName());
     }
+    //avec le pokemon Aquali
+    // ici, je teste  plus de parametres
+    @Test
+    public void getPokemonMetadata_AvecValidIndexAquali() throws PokedexException {
 
+        int indexReal = 133;
+        String nomReal = "Aquali";
+        int attaqueReal = 186;
+        int defenseReal = 168;
+        int enduranceReal = 260;
+
+        Mockito.when(metadataProvider.getPokemonMetadata(133)).thenReturn(new PokemonMetadata(indexReal, nomReal, attaqueReal, defenseReal, enduranceReal));
+        PokemonMetadata metadata2 = metadataProvider.getPokemonMetadata(133);
+        assertNotNull(metadata2);
+        assertEquals("Aquali", metadata2.getName());
+    }
 }
