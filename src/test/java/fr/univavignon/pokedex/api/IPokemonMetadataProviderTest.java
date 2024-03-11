92d9ad68c6b3c7ecb89d507decb51441a998045c
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-11 21:11:37 +0100

    IPokemonFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,85 +1,64 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class IPokemonTrainerFactoryTest {
+public class IPokemonMetadataProviderTest {
 
     @Mock
-    private IPokedexFactory mockPokedexFactory;
+    private IPokemonMetadataProvider metadataProvider;
 
-    @Mock
-    private IPokedex mockPokedex;
-
-    @Mock
-    private IPokemonTrainerFactory factoryTrainerMock;
-
-    @Mock
-    private PokemonTrainer expectTrainerMock;
-
-    @BeforeEach
-    void setUp() {
-        lenient().when(mockPokedexFactory.createPokedex(any(), any())).thenReturn(mockPokedex);
-    }
-
+    // Test pour un index valide avec le Pokémon Bulbizarre
     @Test
-    public void testHandlingOfDuplicateTrainerCreation() {
-        String trainerName = "Misty";
-        Team team = Team.VALOR;
-
-        when(factoryTrainerMock.createTrainer(eq(trainerName), eq(team), eq(mockPokedexFactory)))
-                .thenReturn(expectTrainerMock).thenThrow(new IllegalStateException("Trainer déjà existant."));
-
-        PokemonTrainer firstCreation = factoryTrainerMock.createTrainer(trainerName, team, mockPokedexFactory);
-        assertSame(expectTrainerMock, firstCreation, "Le premier trainer créé doit être retourné.");
-
-        assertThrows(IllegalStateException.class, () -> {
-            factoryTrainerMock.createTrainer(trainerName, team, mockPokedexFactory);
-        }, "La tentative de recréation d'un trainer existant devrait échouer.");
+    public void getPokemonMetadata_AvecValidIndexBulbizarre() throws PokedexException {
+        when(metadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        PokemonMetadata metadata = metadataProvider.getPokemonMetadata(0);
+        assertNotNull(metadata);
+        assertEquals("Bulbizarre", metadata.getName());
     }
 
+    // Test pour un index valide avec le Pokémon Aquali
     @Test
-    public void testTrainerInformationConsistency() {
-        String trainerName = "Brock";
-        Team team = Team.MYSTIC;
+    public void getPokemonMetadata_AvecValidIndexAquali() throws PokedexException {
+        int indexReal = 133;
+        String nomReal = "Aquali";
+        int attaqueReal = 186;
+        int defenseReal = 168;
+        int enduranceReal = 260;
 
-        PokemonTrainer trainer = new PokemonTrainer(trainerName, team, mockPokedex);
-        when(factoryTrainerMock.createTrainer(trainerName, team, mockPokedexFactory)).thenReturn(trainer);
+        when(metadataProvider.getPokemonMetadata(133)).thenReturn(new PokemonMetadata(indexReal, nomReal, attaqueReal, defenseReal, enduranceReal));
+        PokemonMetadata metadata2 = metadataProvider.getPokemonMetadata(133);
 
-        PokemonTrainer createdTrainer = factoryTrainerMock.createTrainer(trainerName, team, mockPokedexFactory);
-
-        assertAll("Trainer information should be consistent",
-                () -> assertEquals(trainerName, createdTrainer.getName()),
-                () -> assertEquals(team, createdTrainer.getTeam()),
-                () -> assertSame(mockPokedex, createdTrainer.getPokedex())
-        );
+        // Vérifications
+        assertNotNull(metadata2, "METADATA ne doit pas être null !!!");
+        assertEquals("Aquali", metadata2.getName(), "Le nom doit être : Aquali !!!");
+        assertEquals(186, metadata2.getAttack(), "Aquali attack doit être : 186 !!!");
+        assertEquals(168, metadata2.getDefense(), "Aquali defense doit être : 168 !!!");
+        assertEquals(260, metadata2.getStamina(), "Aquali endurance doit être : 260 !!!");
     }
-
+    //pour que je teste un index Invalid
+      // sup à 150
     @Test
-    public void testTrainerCreationWithNullParameters() {
-        when(factoryTrainerMock.createTrainer(null, null, null)).thenThrow(new IllegalArgumentException());
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            factoryTrainerMock.createTrainer(null, null, null);
-        }, "La création d'un entraîneur avec des paramètres null devrait lancer une IllegalArgumentException.");
+    public void getPokemonMetadata_invalidIndexSup150() throws PokedexException {
+        when(metadataProvider.getPokemonMetadata(155)).thenThrow(new PokedexException("Invalid index"));
+        int invalidIndex = 155;
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(invalidIndex);
+        }, "si invalid indx lance PodexException");
     }
-
+    // inf à 0
     @Test
-    public void verifyTrainerCreatedIsCorrect() {
-        String nomTrainer = "Lance";
-        Team choixTeam = Team.VALOR;
-
-        lenient().when(factoryTrainerMock.createTrainer(nomTrainer, choixTeam, mockPokedexFactory)).thenReturn(expectTrainerMock);
-
-        PokemonTrainer newTrainer = factoryTrainerMock.createTrainer(nomTrainer, choixTeam, mockPokedexFactory);
-
-        verify(factoryTrainerMock).createTrainer(nomTrainer, choixTeam, mockPokedexFactory);
-
-        assertEquals(expectTrainerMock, newTrainer, "The created trainer should match the expected mock instance.");
+    public void getPokemonMetadata_invalidIndexInfZero() throws PokedexException {
+        when(metadataProvider.getPokemonMetadata(-6)).thenThrow(new PokedexException("Invalid index"));
+        int invalidIndex = -6;
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(invalidIndex);
+        }, "si invalid indx lance PodexException");
     }
+
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,50 +1,12 @@
-package fr.univavignon.pokedex.api;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.verify;
-import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@ExtendWith(MockitoExtension.class)
-public class IPokemonTrainerFactoryTest {
-
-    @Mock
-    private IPokedexFactory mockPokedexFactory;
-
-    @Mock
-    private IPokedex mockPokedex;
-
-    @Mock
-    private IPokemonTrainerFactory factoryTrainerMock;
-
-    @Mock
-    private PokemonTrainer expectTrainerMock;
-
-    @BeforeEach
-    void setUp() {
-
-        lenient().when(mockPokedexFactory.createPokedex(any(), any())).thenReturn(mockPokedex);
-    }
-
-    @Test
-    public void verifyTrainerCreatedIsCorrect() {
-        String nomTrainer = "Misty";
+public void verifyTrainerCreatedIsCorrect() {
+        String nomTrainer = "Lance";
         Team choixTeam = Team.VALOR;
-
 
         lenient().when(factoryTrainerMock.createTrainer(nomTrainer, choixTeam, mockPokedexFactory)).thenReturn(expectTrainerMock);
 
-
-        PokemonTrainer newTrain = factoryTrainerMock.createTrainer(nomTrainer, choixTeam, mockPokedexFactory);
+        PokemonTrainer newTrainer = factoryTrainerMock.createTrainer(nomTrainer, choixTeam, mockPokedexFactory);
 
         verify(factoryTrainerMock).createTrainer(nomTrainer, choixTeam, mockPokedexFactory);
 
-        assertEquals(expectTrainerMock, newTrain, "The created trainer should match the expected mock instance.");
-    }
-}
+        assertEquals(expectTrainerMock, newTrainer, "The created trainer should match the expected mock instance.");
+        }
