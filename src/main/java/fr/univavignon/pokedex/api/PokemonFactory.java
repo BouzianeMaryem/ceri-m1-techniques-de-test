2b473b2ffbf15157fc65778d0ff4ddb9dbdf550e
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 16:02:50 +0100

    readme:imlementation : PokemonTrainerFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -1,6 +1,6 @@
 package fr.univavignon.pokedex.api;
 
-
+// je dois ajouter comments pour generation du javadoc
 public class PokemonFactory implements IPokemonFactory {
 
 
@@ -8,14 +8,15 @@ public class PokemonFactory implements IPokemonFactory {
     public Pokemon createPokemon(final int index, final int cp, final int hp,
                                  final int dust,
                                  final int candy) {
+        int iv = 1;
         PokemonMetadata pokemonMetadata;
+
         try {
             pokemonMetadata =
                     new PokemonMetadataProvider().getPokemonMetadata(index);
         } catch (PokedexException pe) {
             return null;
         }
-        int iv = 1;
 
         return new Pokemon(index, pokemonMetadata.getName(),
                 pokemonMetadata.getAttack(), pokemonMetadata.getDefense(),
