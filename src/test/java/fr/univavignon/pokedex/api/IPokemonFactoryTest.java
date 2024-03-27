9dd83692b650ef9c0e58348bec1cc70b96069afc
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 14:40:54 +0100

    TP4 :Partie 3 : classe PokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -11,8 +11,8 @@ class BulbizarreTest {
     @BeforeAll
     static void setUp() {
         pokemonFactory = new PokemonFactory();
-        expectedBulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-        expectedAquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 5, 1000);
+        expectedBulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 1);
+        expectedAquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 5, 1);
 
     }
 
