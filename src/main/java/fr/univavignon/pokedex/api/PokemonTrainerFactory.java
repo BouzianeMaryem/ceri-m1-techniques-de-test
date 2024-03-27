10506ebe5a4a8d583bac352dd77eb7bb50f8a122
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 11:23:39 +0100

    TP4 :Partie 3 : classe PokemonTrainerFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -2,13 +2,12 @@ package fr.univavignon.pokedex.api;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
+    private final Map<String, PokemonTrainer> trainers = new HashMap<>();
     private final IPokemonMetadataProvider metadataProvider;
     private final IPokemonFactory pokemonFactory;
-    private final Map<String, PokemonTrainer> trainers = new HashMap<>();
 
     public PokemonTrainerFactory(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
         this.metadataProvider = metadataProvider;
@@ -17,19 +16,15 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-        validateParameters(name, team, pokedexFactory);
+        if (name == null || team == null || pokedexFactory == null) {
+            throw new IllegalArgumentException("la creation d'un trainer avec des params null , doit lever une exception !!!");
+        }
         if (trainers.containsKey(name)) {
             throw new IllegalStateException("Trainer existe deja !!");
         }
         IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
-        PokemonTrainer trainer = new PokemonTrainer(name, team, pokedex);
-        trainers.put(name, trainer);
-        return trainer;
-    }
-
-    private void validateParameters(String name, Team team, IPokedexFactory pokedexFactory) {
-        if (name == null || team == null || pokedexFactory == null) {
-            throw new IllegalArgumentException("Les paramètres de création du trainer ne doivent pas être nuls.");
-        }
+        PokemonTrainer newTrainer = new PokemonTrainer(name, team, pokedex);
+        trainers.put(name, newTrainer);
+        return newTrainer;
     }
 }
