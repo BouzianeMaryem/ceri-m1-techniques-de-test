6c942a24ff0f0e1f19f7e353fdd056b9fed84043
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 14:39:39 +0100

    TP4 :Partie 3 : classe PokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,43 +1,44 @@
 package fr.univavignon.pokedex.api;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class IPokemonFactoryTest {
-    IPokemonFactory pokemonFactory = new PokemonFactory();
-    Pokemon bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-    Pokemon aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 5, 1000);
+class BulbizarreTest {
+    static IPokemonFactory pokemonFactory;
+    static Pokemon expectedBulbizarre;
+    static Pokemon expectedAquali;
+    @BeforeAll
+    static void setUp() {
+        pokemonFactory = new PokemonFactory();
+        expectedBulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        expectedAquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 5, 1000);
+
+    }
 
     @Test
     void ShouldReturnNull() {
-        Assertions.assertNull(pokemonFactory.createPokemon(-1, 25, 25, 25, 25));
+        Assertions.assertNull(pokemonFactory.createPokemon(-1, 0, 0, 0, 0));
+    }
+    @Test
+    void testCreateBulbizarre() {
+        Pokemon bulbizarre = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+        Assertions.assertNotNull(bulbizarre);
+        Assertions.assertEquals(expectedBulbizarre.getCp(), bulbizarre.getCp());
+        Assertions.assertEquals(expectedBulbizarre.getHp(), bulbizarre.getHp());
+        Assertions.assertEquals(expectedBulbizarre.getDust(), bulbizarre.getDust());
+        Assertions.assertEquals(expectedBulbizarre.getCandy(), bulbizarre.getCandy());
+        Assertions.assertEquals(expectedBulbizarre.getIv(), bulbizarre.getIv());
     }
 
     @Test
-    void ShouldRetunRightCp() {
-        Assertions.assertEquals(bulbizarre.getCp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCp());
-        Assertions.assertEquals(aquali.getCp(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 5).getCp());
-        Assertions.assertNotEquals(aquali.getCp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCp());
-    }
-
-    @Test
-    void shouldReturnTheRightHp() {
-        Assertions.assertEquals(bulbizarre.getHp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getHp());
-        Assertions.assertEquals(aquali.getHp(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 5).getHp());
-        Assertions.assertNotEquals(aquali.getHp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getHp());
-    }
-
-    @Test
-    void shouldReturnRightDust() {
-        Assertions.assertEquals(bulbizarre.getDust(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getDust());
-        Assertions.assertEquals(aquali.getDust(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 5).getDust());
-        Assertions.assertNotEquals(aquali.getDust(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getDust());
-    }
-
-    @Test
-    void shouldRturnRightCandy() {
-        Assertions.assertEquals(bulbizarre.getCandy(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCandy());
-        Assertions.assertEquals(aquali.getCandy(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 5).getCandy());
-        Assertions.assertNotEquals(aquali.getCandy(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCandy());
+    void testCreateAquali() {
+        Pokemon aquali = pokemonFactory.createPokemon(133, 2729, 202, 5000, 5);
+        Assertions.assertNotNull(aquali);
+        Assertions.assertEquals(expectedAquali.getCp(), aquali.getCp());
+        Assertions.assertEquals(expectedAquali.getHp(), aquali.getHp());
+        Assertions.assertEquals(expectedAquali.getDust(), aquali.getDust());
+        Assertions.assertEquals(expectedAquali.getCandy(), aquali.getCandy());
+        Assertions.assertEquals(expectedAquali.getIv(), aquali.getIv());
     }
 }
