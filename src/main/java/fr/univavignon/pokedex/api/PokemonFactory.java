a563d8200f1535010fbdc3cfe32d781c22d86c1f
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 14:15:30 +0100

    TP4 :Partie 3 : classe PokemonFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -2,28 +2,39 @@ package fr.univavignon.pokedex.api;
 
 import java.util.Random;
 
+
 public class PokemonFactory implements IPokemonFactory {
 
-    private IPokemonMetadataProvider metadataProvider;
+    private final IPokemonMetadataProvider metadataProvider;
 
     public PokemonFactory(IPokemonMetadataProvider metadataProvider) {
         this.metadataProvider = metadataProvider;
     }
 
     @Override
-    public Pokemon createPokemon(final int index, final int cp, final int hp,
-                                 final int dust, final int candy) {
-        PokemonMetadata metadata;
+    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
         try {
-            metadata = metadataProvider.getPokemonMetadata(index);
+            PokemonMetadata metadata = metadataProvider.getPokemonMetadata(index);
+            Random rand = new Random();
+
+            int ivAttaque = rand.nextInt(16);
+            int ivDefense = rand.nextInt(16);
+            int ivEndurance = rand.nextInt(16);
+
+            double iv = calculerIV(ivAttaque, ivDefense, ivEndurance);
+
+            return new Pokemon(index, metadata.getName(), metadata.getAttack() + ivAttaque,
+                    metadata.getDefense() + ivDefense, metadata.getStamina() + ivEndurance,
+                    cp, hp, dust, candy, iv);
+
         } catch (PokedexException e) {
+            System.err.println("Erreur lors de la récupération des métadonnées du Pokémon : " + e.getMessage());
             return null;
         }
+    }
 
-        int iv = 25;
 
-        return new Pokemon(index, metadata.getName(),
-                metadata.getAttack(), metadata.getDefense(),
-                metadata.getStamina(), cp, hp, dust, candy, iv);
+    private double calculerIV(int ivAttaque, int ivDefense, int ivEndurance) {
+        return ((double) (ivAttaque + ivDefense + ivEndurance) / 45) * 100;
     }
 }
