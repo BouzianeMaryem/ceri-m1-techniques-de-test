a7d4750a4a2f23efc4dfbee9910a750ee32f35b7
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 15:28:32 +0100

    TP4 :Partie 3 : classe PokemonFactory

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -1,7 +1,10 @@
 package fr.univavignon.pokedex.api;
-
+/**
+ * Pokemon factory. Implementation of IPodexFactory
+ *
+ * @author Maryem Bouziane
+ */
 public class PokedexFactory implements IPokedexFactory {
-
     @Override
     public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
         return new Pokedex(metadataProvider, pokemonFactory);
