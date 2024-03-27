c17fc915b37d0b2b81f6698e8d2314eeaa7b027c
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 13:52:15 +0100

    TP4 :Partie 3 : classe PokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -3,135 +3,107 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 @ExtendWith(MockitoExtension.class)
 public class IPokemonFactoryTest {
 
     @Mock
     private IPokemonFactory pokemonFactory;
-
-    @Mock
-    private IPokemonMetadataProvider mockMetadataProvider;
-
     private Pokemon bulbizarre;
     private Pokemon aquali;
 
     @BeforeEach
     void setUp() {
-        bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 0.56);
-        aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 1.0);
 
-        lenient().when(pokemonFactory.createPokemon(eq(0), eq(613), eq(64), eq(4000), eq(4))).thenReturn(bulbizarre);
-        lenient().when(pokemonFactory.createPokemon(eq(133), eq(2729), eq(202), eq(5000), eq(4))).thenReturn(aquali);
-        lenient().when(pokemonFactory.createPokemon(eq(-1), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(null);
+        bulbizarre = new Pokemon(1, "Bulbasaur", 126, 100, 90, 600, 100, 4000, 4, 0.56);
+        aquali = new Pokemon(133, "Vaporeon", 186, 168, 260, 2729, 202, 5000, 4, 1.0);
+    }
 
-        lenient().when(mockMetadataProvider.getPokemonMetadata(anyInt())).thenAnswer(invocation -> {
-            int index = invocation.getArgument(0);
-            if (index < 0 || index > 150) {
-                throw new PokedexException("Index hors limites.");
-            }
-            return new PokemonMetadata(index, "TestName", 10, 10, 10);
-        });
+
+    @Test
+    void testCreationPokemon() {
+
+        when(pokemonFactory.createPokemon(eq(1), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(bulbizarre);
+        when(pokemonFactory.createPokemon(eq(133), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(aquali);
+
+        Pokemon bulbasaur = pokemonFactory.createPokemon(1, 600, 100, 5000, 50);
+        assertNotNull(bulbasaur, "Bulbasaur ne devrait pas être null");
+        assertEquals(1, bulbasaur.getIndex(), "L'index de Bulbasaur devrait correspondre");
+        assertEquals("Bulbasaur", bulbasaur.getName(), "Le nom de Bulbasaur devrait correspondre");
+        assertEquals(600, bulbasaur.getCp(), "Les CP de Bulbasaur devraient correspondre");
+        assertEquals(100, bulbasaur.getHp(), "Les HP de Bulbasaur devraient correspondre");
+        assertEquals(0.56, bulbasaur.getIv(), "L'IV de Bulbasaur devrait correspondre");
+        assertEquals(4000, bulbasaur.getDust(), "La poussière du Pokémon devrait être de 4000");
+
+
+        Pokemon vaporeon = pokemonFactory.createPokemon(133, 2729, 202, 5000, 4);
+        assertNotNull(vaporeon, "Vaporeon ne devrait pas être null");
+        assertEquals(133, vaporeon.getIndex(), "L'index de Vaporeon devrait correspondre");
+        assertEquals("Vaporeon", vaporeon.getName(), "Le nom de Vaporeon devrait correspondre");
+        assertEquals(2729, vaporeon.getCp(), "Les CP de Vaporeon devraient correspondre");
+        assertEquals(202, vaporeon.getHp(), "Les HP de Vaporeon devraient correspondre");
+        assertEquals(1.0, vaporeon.getIv(), "L'IV de Vaporeon devrait être parfait");
+        assertEquals(5000, vaporeon.getDust(), "La poussière du Pokémon devrait être de 5000");
+    }
+
+
+    @Test
+    void testConsistencyOfPokemonMetadata() {
+        PokemonMetadata expectedMetadata = new PokemonMetadata(1, "ExpectedName", 50, 50, 50);
+        when(pokemonFactory.createPokemon(eq(1), anyInt(), anyInt(), anyInt(), anyInt()))
+                .thenReturn(new Pokemon(1, "ExpectedName", 50, 50, 50, 500, 50, 1000, 10, 0.5));
+
+        Pokemon createdPokemon = pokemonFactory.createPokemon(1, 500, 50, 1000, 10);
+        assertEquals(expectedMetadata.getIndex(), createdPokemon.getIndex(), "L'index des métadonnées devrait correspondre");
+        assertEquals(expectedMetadata.getName(), createdPokemon.getName(), "Le nom devrait correspondre");
+        assertEquals(expectedMetadata.getAttack(), createdPokemon.getAttack(), "L'attaque devrait correspondre");
+        assertEquals(expectedMetadata.getDefense(), createdPokemon.getDefense(), "La défense devrait correspondre");
+        assertEquals(expectedMetadata.getStamina(), createdPokemon.getStamina(), "L'endurance devrait correspondre");
     }
 
     @Test
-    void testCreationDePokemonAvecParametresValides() {
-        Pokemon pokemonCree1 = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
-        //bulbizarre
-        assertNotNull(pokemonCree1, "Le pokemon créé ne doit pas être nul!!");
-        assertEquals(bulbizarre.getIndex(), pokemonCree1.getIndex(), "L'index du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getName(), pokemonCree1.getName(), "Le nom du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getAttack(), pokemonCree1.getAttack(), "L'attaque du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getDefense(), pokemonCree1.getDefense(), "La défense du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getStamina(), pokemonCree1.getStamina(), "L'endurance du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getCp(), pokemonCree1.getCp(), "Le CP du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getHp(), pokemonCree1.getHp(), "Le HP du pokemon est incorrect !!!");
-        assertEquals(bulbizarre.getDust(), pokemonCree1.getDust(), "La poussière du pokemon est incorrect !!!!!");
-        assertEquals(bulbizarre.getCandy(), pokemonCree1.getCandy(), "Le candy du pokemon est incorrect !!!");
+    void testPokemonCreationWithInvalidValues() {
 
-        //aquali
-        Pokemon pokemonCree2 = pokemonFactory.createPokemon(133, 2729, 202, 5000, 4);
+        when(pokemonFactory.createPokemon(anyInt(), anyInt(), anyInt(), anyInt(), anyInt())).thenThrow(IllegalArgumentException.class);
 
-        assertNotNull(pokemonCree2, "Le pokemon créé ne doit pas être nul!!");
-        assertEquals(aquali.getIndex(), pokemonCree2.getIndex(), "L'index du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getName(), pokemonCree2.getName(), "Le nom du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getAttack(), pokemonCree2.getAttack(), "L'attaque du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getDefense(), pokemonCree2.getDefense(), "La défense du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getStamina(), pokemonCree2.getStamina(), "L'endurance du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getCp(), pokemonCree2.getCp(), "Le CP du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getHp(), pokemonCree2.getHp(), "Le HP du pokemon est incorrect !!!");
-        assertEquals(aquali.getDust(), pokemonCree2.getDust(), "La poussière du pokemon est incorrect !!!!!");
-        assertEquals(aquali.getCandy(), pokemonCree2.getCandy(), "Le candy du pokemon est incorrect !!!");
-
+        assertThrows(IllegalArgumentException.class, () -> pokemonFactory.createPokemon(1, -1, -1, -1, -1),
+                "Devrait lancer IllegalArgumentException pour des valeurs invalides");
     }
 
     @Test
-    void testCreationDePokemonAvecParametresInvalidesdoitRenvoyerNull() {
-        assertNull(pokemonFactory.createPokemon(-1, 10000, 10000, 10000, 10000), "un pokemon cree avec des params invelides doit renvoyer null !!!");
-    }
-    // je ne peux pas faire un assertthrow car la signature de la fonction ne leve pas une exception
-    //donc au cas des pamars invalide , je crée une instance null
-    @Test
-    void testCreationDePokemonAvecIndexInferieurAZeroDoitLeverException() {
-        assertNull(pokemonFactory.createPokemon(-1, 100, 100, 1000, 10), "un pokemon cree avec indx inf 0 doit renvoyer null !!!");
+    void testCreationDePokemonRenvoieNull() {
+
+        when(pokemonFactory.createPokemon(eq(-2), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(null);
+        assertNull(pokemonFactory.createPokemon(-2, 10000, 10000, 10000, 10000), "La création d'un Pokemon avec des paramètres invalides devrait renvoyer null");
     }
 
     @Test
-    void testCreationDePokemonAvecIndexSuperieurACentCinquanteDoitLeverException() {
-        assertNull(pokemonFactory.createPokemon(152, 100, 100, 1000, 10), "un pokemon cree avec indx sup à 150  doit renvoyer null !!!");
+    void testLeHpDuPokemonCorrectementDefini() {
+
+        when(pokemonFactory.createPokemon(eq(1), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(bulbizarre);
+        when(pokemonFactory.createPokemon(eq(133), anyInt(), anyInt(), anyInt(), anyInt())).thenReturn(aquali);
+
+        Pokemon bulbasaurTest = pokemonFactory.createPokemon(1, 600, 60, 3000, 4);
+        assertNotNull(bulbasaurTest, "bulbasaurTest ne devrait pas être null");
+        assertEquals(bulbizarre.getHp(), bulbasaurTest.getHp(), "Le HP doit correspondre à celui de Bulbasaur");
+
+
+        Pokemon aqualiTest = pokemonFactory.createPokemon(133, 2500, 200, 5000, 5);
+        assertNotNull(aqualiTest, "aqualiTest ne devrait pas être null");
+        assertEquals(aquali.getHp(), aqualiTest.getHp(), "Le HP doit correspondre à celui de Vaporeon");
     }
 
     @Test
-    void testLeHpDuPokemonEstCorrectementDefini() {
-        assertEquals(bulbizarre.getHp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getHp(), "Le HP doit correspondre à celui de  Bulbizarre!!");
-        assertEquals(aquali.getHp(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getHp(), "Le HP doit correspondre à celui de  Aquali!!");
+    void testPokemonCandyGetter() {
+        assertEquals(4, bulbizarre.getCandy(), "Le nombre de candies de Bulbasaur devrait être correct");
+        assertEquals(4, aquali.getCandy(), "Le nombre de candies de Vaporeon devrait être correct");
     }
 
-    @Test
-    void testLeCandyDuPokemonEstCorrectementDefini() {
-        assertEquals(bulbizarre.getCandy(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCandy(), "Le candy doit correspondre à celui de  Bulbizarre!!");
-        assertEquals(aquali.getCandy(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getCandy(), "Le candy doit correspondre à celui de  Aquali!!");
-    }
 
-    @Test
-    void testLeCpDuPokemonEstCorrectementDefini() {
-        assertEquals(bulbizarre.getCp(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getCp(), "Le CP doit correspondre à celui de  Bulbizarre!!");
-        assertEquals(aquali.getCp(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getCp(), "Le CP doit correspondre à celui de  Aquali!!");
-    }
-
-    @Test
-    void testLaDustDuPokemonEstCorrectementDefinie() {
-        assertEquals(bulbizarre.getDust(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getDust(), "la poussière doit correspondre à celui de  Bulbizarre!!");
-        assertEquals(aquali.getDust(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getDust(), "la poussière doit correspondre à celui de  Aquali!!");
-    }
-
-    @Test
-    void testLaIvDuPokemonEstCorrectementDefinie() {
-        assertEquals(bulbizarre.getIv(), pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getIv(), "la IV pourcentage  doit correspondre à celui de  Bulbizarre!!");
-        assertEquals(aquali.getIv(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getIv(), "la IV pourcentage doit correspondre à celui de  Aquali!!");
-    }
-
-    @Test
-    void testPokemonCreationWithInvalidIndexThrowsException() {
-        assertThrows(PokedexException.class, () -> {
-            pokemonFactory.createPokemon(-1, 100, 100, 1000, 10);
-        }, "La création d'un Pokémon avec un index invalide doit lever une PokedexException.");
-
-        assertThrows(PokedexException.class, () -> {
-            pokemonFactory.createPokemon(151, 100, 100, 1000, 10);
-        }, "La création d'un Pokémon avec un index hors limites doit lever une PokedexException.");
-    }
-
-    @Test
-    void testPokemonCreationWithValidIndexSetsIVCorrectly() {
-        Pokemon pokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
-        assertEquals(25, pokemon.getIv(), "L'IV du Pokémon créé doit être fixé à 25.");
-    }
 
 }
