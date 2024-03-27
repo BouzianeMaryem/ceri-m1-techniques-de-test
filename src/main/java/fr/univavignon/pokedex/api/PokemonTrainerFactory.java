d6faa31ac42e4b914619378ae7e24ab76299707f
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 11:11:38 +0100

    TP4 :Partie 3 : classe PokemonTrainerFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -0,0 +1,17 @@
+package fr.univavignon.pokedex.api;
+
+public class PokemonTrainerFactory implements IPokemonTrainerFactory {
+
+    private final IPokemonMetadataProvider metadataProvider;
+    private final IPokemonFactory pokemonFactory;
+
+    public PokemonTrainerFactory(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
+        this.metadataProvider = metadataProvider;
+        this.pokemonFactory = pokemonFactory;
+    }
+
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+        return new PokemonTrainer(name, team, pokedexFactory.createPokedex(metadataProvider, pokemonFactory));
+    }
+}
