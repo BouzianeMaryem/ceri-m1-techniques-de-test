1d130f314662ce0c67d105777d44b81336267ee9
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-03-27 11:17:41 +0100

    TP4 :Partie 3 : classe PokemonTrainerFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -1,9 +1,14 @@
 package fr.univavignon.pokedex.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
     private final IPokemonMetadataProvider metadataProvider;
     private final IPokemonFactory pokemonFactory;
+    private final Map<String, PokemonTrainer> trainers = new HashMap<>();
 
     public PokemonTrainerFactory(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
         this.metadataProvider = metadataProvider;
@@ -12,6 +17,19 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory {
 
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
-        return new PokemonTrainer(name, team, pokedexFactory.createPokedex(metadataProvider, pokemonFactory));
+        validateParameters(name, team, pokedexFactory);
+        if (trainers.containsKey(name)) {
+            throw new IllegalStateException("Trainer existe deja !!");
+        }
+        IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        PokemonTrainer trainer = new PokemonTrainer(name, team, pokedex);
+        trainers.put(name, trainer);
+        return trainer;
+    }
+
+    private void validateParameters(String name, Team team, IPokedexFactory pokedexFactory) {
+        if (name == null || team == null || pokedexFactory == null) {
+            throw new IllegalArgumentException("Les paramètres de création du trainer ne doivent pas être nuls.");
+        }
     }
 }
