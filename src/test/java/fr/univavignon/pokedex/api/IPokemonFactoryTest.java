7c1c63e8e1896e2712adbfa5767e325ec77a2e12
BouzianeMaryem <maryem.bouziane2002@gmail.com>
2024-04-02 23:18:16 +0200

    TP4 :Partie 3 : ajout IPokemon Factory Test

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,5 +1,6 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
